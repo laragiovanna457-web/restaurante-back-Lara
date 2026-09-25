080f50ae6292f6605e3c4f95206f8afa7880b700
laragiovanna457-web <lara.giovanna457@gmail.com>
2026-09-25 17:41:18 -0300

    Alterando coisinhas

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,10 @@ const mysql = require("mysql2/promise")
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
 
     ssl: {
         rejectUnauthorized: false
@@ -19,4 +17,4 @@ const db = mysql.createPool({
 
 })
 
-module.exports = db
+module.exports = db;
--- src/server.js
+++ src/server.js
@@ -10,7 +10,10 @@ const PORT = 3001
 
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'https://restaurante-front-lara.vercel.app',
+    credentials: true
+}));
 
 app.get("/",(req,res)=>{
     res.json({
